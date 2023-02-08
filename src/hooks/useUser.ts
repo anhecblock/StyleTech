3b55f28f517e70eacf26e19366712d46e566ade7
AnaBelén <anhecblock1990@gmail.com>
2023-02-08 15:08:26 +0100

    Added slice

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,52 @@
+import {
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../app/hooks';
+import { auth } from '../firebase/firebase';
+import { LoggedUser, loginUserActionCreator } from '../store/user/userSlice';
+
+const useUser = () => {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const userLogin = async (email: string, password: string) => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            const user: LoggedUser = {
+                ...userCredential.user,
+                isLogged: false,
+            };
+            const token = await auth.currentUser?.getIdToken();
+
+            dispatch(loginUserActionCreator(user));
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            localStorage.setItem('token', token!);
+            navigate('/');
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const userRegister = async (email: string, password: string) => {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            const user = userCredential.user;
+            console.log('usuario registrado', user);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return { userLogin, userRegister };
+};
+
+export default useUser;
---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import { Product } from '../interfaces/interfaces';
+
+export const articulos: Array<Product> = [];
+
+export const initialArticle: Product = {
+    category: '',
+    description: '',
+    id: '',
+    image: '',
+    price: 0,
+    title: '',
+};
