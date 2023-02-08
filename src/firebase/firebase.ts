01c2ffed85e34ee9c9e3db5bf4c4db13fe570cdb
AnaBelén <anhecblock1990@gmail.com>
2023-02-08 14:59:53 +0100

    Added  firebase auth

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: 'AIzaSyDSv0qCB7Y61Crod8l-T54HZIYvlpnFcSo',
     authDomain: 'proyecto-final-bootcamp-18e38.firebaseapp.com',
+    databaseURL:
+        'https://proyecto-final-bootcamp-18e38-default-rtdb.firebaseio.com/',
     projectId: 'proyecto-final-bootcamp-18e38',
     storageBucket: 'proyecto-final-bootcamp-18e38.appspot.com',
     messagingSenderId: '465882818232',
     appId: '1:465882818232:web:256dae5105ee1a4f13f64d',
 };
 
-// Initialize Firebase
-export const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
