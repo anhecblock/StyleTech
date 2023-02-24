1a4137ae871da41d29c317935a2bbca8174f1a68
AnaBelén <anhecblock1990@gmail.com>
2023-02-24 13:23:58 +0100

    test loader

--- src/components/Navbar/loader/Loader.tsx
+++ src/components/Navbar/loader/Loader.tsx
@@ -3,7 +3,9 @@ import './loaderStyled.css';
 const Loader = () => {
     return (
         <div className="loader">
-            <span className="loader__spinner"> </span>
+            <span className="loader__spinner" data-testid="spinner">
+                {' '}
+            </span>
         </div>
     );
 };
